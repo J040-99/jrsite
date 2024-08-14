48e8f47aa475defefda69daae07c7ca7066b993f
J040-99 <2020micaelo@gmail.com>
2024-08-14 03:40:27 +0100

    v2 3:40_14/08

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Bem-vindo ao Meu Site!');
+    console.log('Bem-vindo ao meu ite!');
 });
+
+function openNav() {
+    var sidebar = document.getElementById("sidebar");
+    if (sidebar.style.width === "180px") {
+        sidebar.style.width = "0";
+        sidebarn.style.fontSize = "0x";
+    } else {
+        sidebar.style.width = "180px";
+        sidebarn.style.fontSize = "25px";
+    }
+}
