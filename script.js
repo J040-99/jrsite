e9f49f2a573e8dccf459fee9e389294af427a04e
J040-99 <2020micaelo@gmail.com>
2024-09-18 00:25:54 +0100

    modelo de testes
    versão original

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Bem-vindo ao meu site!');
+    console.log('Bem-vindo ao meu ite!');
 });
 
 function openNav() {
     var sidebar = document.getElementById("sidebar");
-    if (sidebar.style.width == "180px") {
+    if (sidebar.style.width === "180px") {
         sidebar.style.width = "0";
         sidebarn.style.fontSize = "0x";
     } else {
